a8b324929626607b10e99cadb8958ae5ed6a1169
tpalocCobiro <tomasz.paloc@cobiro.com>
2021-11-18 06:26:07 +0100

    Added event_log

---
+++ src/Entity/EventLog.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventLogRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EventLogRepository::class)
+ */
+class EventLog
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $eventType;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productId;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEventType(): ?string
+    {
+        return $this->eventType;
+    }
+
+    public function setEventType(string $eventType): self
+    {
+        $this->eventType = $eventType;
+
+        return $this;
+    }
+
+    public function getProductId(): ?int
+    {
+        return $this->productId;
+    }
+
+    public function setProductId(int $productId): self
+    {
+        $this->productId = $productId;
+
+        return $this;
+    }
+}
